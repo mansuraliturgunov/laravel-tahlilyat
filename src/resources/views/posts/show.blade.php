4cfa8d134ccf1e193233ba9d70cf875fcf66f197
mansuraliturgunov <masnuraliturgunov6@gmail.com>
2026-09-25 11:51:38 +0500

    yangi kodlar qoshilidi

--- src/resources/views/posts/show.blade.php
+++ src/resources/views/posts/show.blade.php
@@ -22,6 +22,26 @@
                 <button class="btn btn-sm btn-outline-danger" type="submit">Delete Post</button>
             </form>
         </div>
+        <div class="container mt-5">
+
+        
+        <form action='{{ route('posts.store') }}' method="POST" >
+            <div class="panel-header">
+                @csrf
+            </div>
+
+                <div class="col-12"><label class="form-label" for="body">Izohlar uchun</label>
+                    <textarea class="form-control" name="body" rows="4" placeholder="post haqida fikr bildirishingiz mumkin :)"></textarea>
+                </div>
+            </div>
+            <div class="d-flex flex-wrap justify-content-end gap-2 mt-4">
+                <button class="btn btn-primary" type="submit">
+                    <i class="bi bi-person-check" aria-hidden="true"></i>
+                    Saqlash
+                </button>
+            </div>
+        </form>
+        </div>
         <div class="mb-5 mt-5">
             <a href="{{ route('posts.index') }}" class="btn btn-primary btn-lg px-4">Yangi Postlar</a>
         </div>
